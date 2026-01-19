2706e2fe2beb401b9f81e18e092d455f0a7a67ee
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-01-19 09:32:13 +0800

    update

--- index.js
+++ index.js
@@ -135,16 +135,16 @@ async function renderGraph(data) {
     const y = (1 - processedData[i] / range) * height;
     points.push({ x, y });
   }
-
+  const padding = 2;
   const linearGradient = `<linearGradient id="lingrad" gradientUnits="userSpaceOnUse" x1="${width / 2}" y1="0" x2="${width / 2}" y2="${height}"><stop offset="0%" stop-color="rgba(86, 171, 90, 0.3)" /><stop offset="73%" stop-color="rgba(86, 171, 90, 0.09)" /><stop offset="100%" stop-color="rgba(86, 171, 90, 0)" /></linearGradient>`;
   const pathData = [`M0,${height + 10}`].concat(segmentsToPath(simplifyPath(points, 0.8), 'L', 'L')).concat([`L${width},${height + 10}`, `L${0},${height + 10}`, 'Z']);
   const path = `<path d="${pathData.join(' ')}" stroke="#56ab5a" stroke-width="${8 / 9}" fill="url(#lingrad)"/>`;
-  const svgText = `<svg stroke-miterlimit="10" style="fill-rule: nonzero; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round" version="1.1" viewBox="0 0 ${width} ${height}" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>${linearGradient}</defs><g width="${width}" height="${height}" transform="translate(${0} ${0})">${path}</g></svg>`;
+  const svgText = `<svg stroke-miterlimit="10" style="fill-rule: nonzero; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round" version="1.1" viewBox="0 0 ${width - padding * 2} ${height - padding * 2}" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>${linearGradient}</defs><g width="${width}" height="${height}" transform="translate(${-1 * padding} ${-1 * padding})">${path}</g></svg>`;
 
   for (const scale of [3, 6, 12, 15]) {
     const fileName = `contribution_graph@${scale}x`;
     const outputFilePath = `${imagesDir}${fileName}.png`;
-    await rasterize(svgText, outputFilePath, width * scale, height * scale, 1);
+    await rasterize(svgText, outputFilePath, width * scale, height * scale, 2);
   }
 }
 
