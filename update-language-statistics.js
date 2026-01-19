abf1e9b7840d6b8534dc51c6359dc2303fe0856e
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-01-19 17:44:36 +0800

    update

--- update-language-statistics.js
+++ update-language-statistics.js
@@ -21,7 +21,7 @@ function sha256N(content, n) {
 }
 
 async function getLanguageStatistics() {
-  const limit = 64;
+  const limit = 16;
   const hashList = [];
   const updateList = [];
   let index = 0;
