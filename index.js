9ad58f3f696538534ca10c7c12650d0eb837f372
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-01-19 09:48:02 +0800

    update

--- index.js
+++ index.js
@@ -66,15 +66,15 @@ async function getContributionData() {
 }
 
 function smoothArray(array) {
-  const array_length = array.length;
-  const result_array = [];
-  for (let i = 1; i < array_length; i += 3) {
+  const length = array.length;
+  const result = [];
+  for (let i = 1; i < length; i += 3) {
     const curr = array[i];
     const prev = array[i - 1] || curr;
     const next = array[i + 1] || curr;
-    result_array.push((prev + curr + next) / 3);
+    result.push((prev + curr + next) / 3);
   }
-  return result_array;
+  return result;
 }
 
 async function rasterize(svgText, outputPath, width, height, scale) {
@@ -135,11 +135,13 @@ async function renderGraph(data) {
     const y = (1 - processedData[i] / range) * height;
     points.push({ x, y });
   }
-  const padding = 2;
+  const padding = -1;
   const linearGradient = `<linearGradient id="lingrad" gradientUnits="userSpaceOnUse" x1="${width / 2}" y1="0" x2="${width / 2}" y2="${height}"><stop offset="0%" stop-color="rgba(86, 171, 90, 0.3)" /><stop offset="73%" stop-color="rgba(86, 171, 90, 0.09)" /><stop offset="100%" stop-color="rgba(86, 171, 90, 0)" /></linearGradient>`;
-  const pathData = [`M0,${height + 10}`].concat(segmentsToPath(simplifyPath(points, 0.8), 'L', 'L')).concat([`L${width},${height + 10}`, `L${0},${height + 10}`, 'Z']);
-  const path = `<path d="${pathData.join(' ')}" stroke="#56ab5a" stroke-width="${8 / 9}" fill="url(#lingrad)"/>`;
-  const svgText = `<svg stroke-miterlimit="10" style="fill-rule: nonzero; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round" version="1.1" viewBox="0 0 ${width - padding * 2} ${height - padding * 2}" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>${linearGradient}</defs><g width="${width}" height="${height}" transform="translate(${-1 * padding} ${-1 * padding})">${path}</g></svg>`;
+  const backgroundPathData = [`M0,${height + 10}`].concat(segmentsToPath(simplifyPath(points, 0.8), 'L', 'L')).concat([`L${width},${height + 10}`, `L${0},${height + 10}`, 'Z']);
+  const background = `<path d="${backgroundPathData.join(' ')}" fill="url(#lingrad)"/>`;
+  const curvePathData = segmentsToPath(simplifyPath(points, 0.8), 'M', 'M');
+  const curve = `<path d="${curvePathData.join(' ')}" stroke="#56ab5a" stroke-width="${8 / 9}" />`;
+  const svgText = `<svg stroke-miterlimit="10" style="fill-rule: nonzero; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round" version="1.1" viewBox="0 0 ${width + padding * 2} ${height + padding * 2}" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>${linearGradient}</defs><g width="${width}" height="${height}" transform="translate(${padding} ${padding})">${background}</g></svg>`;
 
   for (const scale of [3, 6, 12, 15]) {
     const fileName = `contribution_graph@${scale}x`;
