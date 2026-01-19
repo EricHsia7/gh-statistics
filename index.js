4f890ab466f8b5802a4f282fb8a6add135f6b034
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-01-19 09:59:25 +0800

    update

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ async function renderGraph(data) {
   const linearGradient = `<linearGradient id="lingrad" gradientUnits="userSpaceOnUse" x1="${width / 2}" y1="0" x2="${width / 2}" y2="${height}"><stop offset="0%" stop-color="rgba(86, 171, 90, 0.3)" /><stop offset="73%" stop-color="rgba(86, 171, 90, 0.09)" /><stop offset="100%" stop-color="rgba(86, 171, 90, 0)" /></linearGradient>`;
   const backgroundPathData = [`M0,${height + 10}`].concat(segmentsToPath(simplifyPath(points, 0.8), 'L', 'L')).concat([`L${width},${height + 10}`, `L${0},${height + 10}`, 'Z']);
   const background = `<path d="${backgroundPathData.join(' ')}" fill="url(#lingrad)"/>`;
-  const curvePathData = segmentsToPath(simplifyPath(points, 0.8), 'M', 'L').concat(['Z']);
+  const curvePathData = segmentsToPath(simplifyPath(points, 0.8), 'M', 'L');
   const curve = `<path d="${curvePathData.join(' ')}" stroke="#56ab5a" stroke-width="${7 / 9}" fill="none"/>`;
   const svgText = `<svg stroke-miterlimit="10" style="fill-rule: nonzero; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round" version="1.1" viewBox="0 0 ${width + padding * 2} ${height + padding * 2}" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>${linearGradient}</defs><g width="${width}" height="${height}" transform="translate(${padding} ${padding})">${background}${curve}</g></svg>`;
 
